52daf2f80a0145325b254cd517ed048dd5454e68
Maanvik Poddar <maanvikpoddar@gmail.com>
2026-02-02 15:07:23 -0700

    frontend Bases

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import {Routes, Route, Navigate} from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './pages/components/Navbar';
+import css from './styles/css/Home.css'
 
 export default function App() {
   return (
     <div className="app">
+      
       <Navbar />
 
       <main>
--- client/src/pages/components/Navbar.jsx
+++ client/src/pages/components/Navbar.jsx
@@ -4,8 +4,24 @@ export default function Navbar() {
     return (
         <header className="nav">
             <nav className="nav-inner">
-                <div className="brand">Student Hub</div>
-                <NavLink className={({isActive}) => "link" + (isActive ? " active" : "")} to="/">Home</NavLink>
+                <div className="logo-section">
+                    <div className="logo"></div>
+                    <div className="brand">3D Print Store</div>
+                </div>
+                
+                <div className="menu">
+                    <div className="order-menu">
+                        <NavLink className={({isActive}) => "link" + (isActive ? " active" : "")} to="/home">Home</NavLink>
+                        <NavLink className={({isActive}) => "link" + (isActive ? " active" : "")} to="/orders">Orders</NavLink>
+                        <NavLink className={({isActive}) => "link" + (isActive ? " active" : "")} to="/custom-orders">Custom Orders</NavLink>
+                    </div>
+
+                    <div className="personal-menu">
+                        <NavLink className={({isActive}) => "link" + (isActive ? " active" : "")} to="/cart">Cart</NavLink>
+                        <NavLink className={({isActive}) => "link" + (isActive ? " active" : "")} to="/login">Login</NavLink>
+                    </div>
+                    
+                </div>
             </nav>
         </header>
     )
