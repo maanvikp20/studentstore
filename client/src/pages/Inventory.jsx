0af0702ad6e7d68d46a109fffdfb9ef54e822b1e
Montgomery Mcdonald <60453662+MontgomeryMcdonald@users.noreply.github.com>
2026-02-06 17:24:51 -0700

    Inventory.jsx work
    not done yet

--- client/src/pages/Inventory.jsx
+++ client/src/pages/Inventory.jsx
@@ -2,9 +2,15 @@ const Inventory = () => {
   const getProducts = async () => {
     try {
       const response = await fetch("http://localhost:5000/api/products")
-      
+      if(!response.ok){
+        throw new Error("failed to fetch products")
+      }
+      const data = await response.json()
+      return data
     } catch (error) {
-      
+      console.error("Error fetching products:", error)
+      throw error
+    }
     }
   }
   return (
